cf0df0cd1494e1307b4e03d0019204d2cf01aad2
shadow01a <141255649+shadow01a@users.noreply.github.com>
2026-06-24 01:33:39 +0800

    feat：添加agenttools

---
+++ ling_chat/core/agent_tools/__init__.py
@@ -0,0 +1,5 @@
+from ling_chat.core.agent_tools.registry import SimpleToolRegistry
+from ling_chat.core.agent_tools.runner import SimpleAgentRunner
+from ling_chat.core.agent_tools.spec import ToolSpec
+
+__all__ = ["SimpleAgentRunner", "SimpleToolRegistry", "ToolSpec"]
